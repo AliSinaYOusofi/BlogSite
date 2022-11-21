ab47c082731ad9adae9bf1119bb52d449ff35004
dev-ethar <tinayousofiali@gmail.com>
2022-11-21 15:06:48 -0800

    login front-end part is done

--- blogging-website/components/Navbar/Navbar.jsx
+++ blogging-website/components/Navbar/Navbar.jsx
@@ -1,5 +1,5 @@
 "use client"; // useing csr
-
+import Link from 'next/link';
 import React, { useEffect, useState } from 'react'
 
 export default  function Navbar() {
@@ -10,35 +10,34 @@ export default  function Navbar() {
     // for sticky position to work you must specify the bounds that
     // it should stick to. like: top-0;
 
+
+    // adding box shadow when we react 150px from the top
     useEffect( () => {
         window.onscroll = () => {
             setScrollY(window.scrollY);
         }
     }, [window.scrollY]);
 
-    console.log(scrollY);
-
-    console.log(document.body.scrollTop);
     return ( 
         <nav className="sticky mx-auto top-0 border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-900" style={{boxShadow: scrollY >= 150 ? "1px 1px 1px 1px black" : ""}}>
             <div className="container  flex flex-wrap items-center justify-between mx-auto">
-                <a href="https://flowbite.com/" class="flex items-center">
+                <a href="https://flowbite.com/" className="flex items-center">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-10 h-8 text-white">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
                 </svg>
 
-                    <span class="self-center text-xl font-semibold whitespace-nowrap dark:text-white">MeStory</span>
+                    <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">MeStory</span>
                 </a>
                 <div className="flex md:order-2">
-                    <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
+                    <button type="button" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Get started</button>
                     <button onClick={() => setDropdown(previouse => ! previouse)} data-collapse-toggle="navbar-cta" type="button" className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600" aria-controls="navbar-cta" aria-expanded="false">
-                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clip-rule="evenodd"></path></svg>
+                        <svg className="w-6 h-6" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
                     </button>
                 </div>
                 <div className="items-center  justify-between hidden  w-full md:flex md:w-auto md:order-1" id="navbar-cta">
                     <ul className="flex divide-dotted flex-col p-4 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
                         <li className="">
-                            <a href="#" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Sign In</a>
+                            <Link href="/login" className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Sign In</Link>
                         </li>
                         <li>
                             <a href="#" className="block text py-2 pl-3 pr-4 text-gray-700 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Our Story</a>
