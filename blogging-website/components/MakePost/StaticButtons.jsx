c7cfa29185eeb33cbe68969ec65615fdd77b9e70
dev-ethar <tinayousofiali@gmail.com>
2022-11-28 01:16:44 -0800

    making an image

---
+++ blogging-website/components/MakePost/StaticButtons.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function StaticButton() {
+    return (
+        <button type="button" class="text-white bg-blue-700 hover:bg-blue-800 font-medium rounded-lg text-sm px-6 py-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Post</button>
+    );
+}
