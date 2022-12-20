69c4fea31cd0e95a484f7057c1f9aec6d43d4aca
dev-ethar <tinayousofiali@gmail.com>
2022-12-20 10:19:40 -0800

    starting the main page for view

---
+++ blogging-website/app/make_post/layout.jsx
@@ -0,0 +1,12 @@
+import UserNavbar from '../../components/UserHomePage/UserNavbar';
+import Footer from '../../components/Footer/Footer';
+
+export default function Layout({children}) {
+    return (
+       <>
+            <UserNavbar />
+                {children}
+            <Footer />
+       </>
+    );
+}
