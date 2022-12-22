79f70badd0f8da9a3c863d10b3b48934e4f61ee9
dev-ethar <tinayousofiali@gmail.com>
2022-12-22 14:46:04 -0800

    message

--- blogging-website/components/Navbar/Navbar.jsx
+++ blogging-website/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 "use client"; // useing csr
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import React, {  useState } from 'react'
 
 export default  function Navbar() {
     
