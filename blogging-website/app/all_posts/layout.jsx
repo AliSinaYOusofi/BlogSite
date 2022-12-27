a7470edcf04f447775c9160eea13845b8410e695
dev-ethar <tinayousofiali@gmail.com>
2022-12-27 10:38:00 -0800

    following bug issue

--- blogging-website/app/all_posts/layout.jsx
+++ blogging-website/app/all_posts/layout.jsx
@@ -1,16 +1,28 @@
 "use client";
 
+import { useRouter } from 'next/navigation';
 import React from 'react'
 import Footer from '../../components/Footer/Footer';
 import UserNavbar from '../../components/UserHomePage/UserNavbar';
+import { useSpacexProvider } from '../../context/appContext';
 
 export default function Layout({children}) {
 
+    const {token} = useSpacexProvider();
+    const router = useRouter();
+
     return (
         <>
-            <UserNavbar />
-            {children}
-            <Footer />
+        {
+            token?
+            <div>
+
+                <UserNavbar />
+                {children}
+                <Footer />
+            </div>
+            : router.push("/login")
+        }
         </>
     )
 }
